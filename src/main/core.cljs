62839278228eff4e2cb41f3113ffbc9a8c755d7c
nharsch <nigelharsch@gmail.com>
2021-04-07 03:13:56 -0400

    audio loaded, not sure why it doesn't play

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -8,25 +8,16 @@
 
 (defn start-audio [e]
   (println "audio started")
-  (defonce AudioContext (or js/window.AudioContext js/window.webkitAudioContext))
+  (defonce AudioContext js/window.AudioContext)
   (defonce context (AudioContext.))
   (defonce audio-element (. js/document (querySelector "audio")))
   (defonce cow-track
-    (. context (createMediaElementSource audio-element)))
-  ((. cow-track connect) (. context destination))
+    (. context createMediaElementSource audio-element))
+  (defonce gain (.createGain context))
+  (set! (.-value gain) 2)
+  (.connect (.connect cow-track gain) (. context -destination))
   (.play audio-element)
-)
-
-
-(println "test")
-
-;; (. cow-track (.destination context))
-
-;; (->
-;;  (bach/sample "/assets/audio/cow.wav")
-;;  (bach/connect-> bach/destination)
-;;  (bach/run-with context (bach/current-time context) 1.0))
-
+  )
 (defonce start-button (d/getElement "start-button"))
 (events/listen
  (d/getElement "start-audio")
