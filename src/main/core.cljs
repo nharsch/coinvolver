8a84336f6ee4c80394a188516d6fd531b49a6da5
nharsch <nigelharsch@gmail.com>
2021-04-07 02:44:49 -0400

    trying to play sound, no luck

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -1,1 +1,35 @@
-(ns app.main.core)
+(ns core
+  (:require
+   [goog.dom :as d]
+   [goog.events :as events]
+   [cljs-bach.synthesis :as bach]
+   ))
+
+
+(defn start-audio [e]
+  (println "audio started")
+  (defonce AudioContext (or js/window.AudioContext js/window.webkitAudioContext))
+  (defonce context (AudioContext.))
+  (defonce audio-element (. js/document (querySelector "audio")))
+  (defonce cow-track
+    (. context (createMediaElementSource audio-element)))
+  ((. cow-track connect) (. context destination))
+  (.play audio-element)
+)
+
+
+(println "test")
+
+;; (. cow-track (.destination context))
+
+;; (->
+;;  (bach/sample "/assets/audio/cow.wav")
+;;  (bach/connect-> bach/destination)
+;;  (bach/run-with context (bach/current-time context) 1.0))
+
+(defonce start-button (d/getElement "start-button"))
+(events/listen
+ (d/getElement "start-audio")
+ (.-CLICK events/EventType)
+ start-audio
+ )
